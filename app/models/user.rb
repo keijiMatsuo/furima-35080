ce3f131ecfdcdbb630a62407e5a2c0fc6cc4aa5e
ojyussan <matsuoexpress@gmail.com>
2021-04-15 18:55:20 +0900

    presence: true重複解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :birth_day,       presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
 
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-    validates :last_name
-    validates :first_name
-  end
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
   
-  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/} do
-    validates :last_name_kana
-    validates :first_name_kana
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
 end
