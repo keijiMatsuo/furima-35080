a44aae124f671868d9a8f79314f3248b6a2d216e
ojyussan <matsuoexpress@gmail.com>
2021-04-15 19:18:30 +0900

    routes.rbの重複記述削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
     validates :nickname
     validates :birth_day
 
-    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
       validates :last_name
       validates :first_name
     end
-  
-    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
       validates :last_name_kana
       validates :first_name_kana
     end
   end
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
