383c88b908fc744e0d28afb48d1b7ada8a7c157a
ojyussan <matsuoexpress@gmail.com>
2021-04-19 18:02:58 +0900

    テストコード実装

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :listing do
-    
+
+    after(:build) do |listing|
+      listing.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    product_name       {"テスト"}
+    description        {"ご覧頂きありがとうございます。"}
+    category           {Category.all.sample}
+    status             {Status.all.sample}
+    shipping_burden    {ShippingBurden.all.sample}
+    prefectures        {Prefectures.all.sample}
+    shipping_days      {ShippingDays.all.sample}
+    price              {2000}
+
+    association :user
   end
 end
