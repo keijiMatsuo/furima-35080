27ed4ae646d876011c5640a3c91dccb310887ad1
ojyussan <matsuoexpress@gmail.com>
2021-04-19 20:33:22 +0900

    console.logの記述削除

--- app/javascript/listings.js
+++ app/javascript/listings.js
@@ -2,17 +2,14 @@ function listings (){
   const itemPrice = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
     const inputPrice = itemPrice.value;
-    console.log(inputPrice);
 
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = (Math.floor(inputPrice * 0.1));
-    console.log(addTaxPrice);
 
     const profit = document.getElementById("profit");
     const salesCommission = inputPrice * 0.1
     console.log(salesCommission)
     profit.innerHTML = (Math.floor(inputPrice - salesCommission));
-    console.log(profit);
   });
 }
 window.addEventListener('load', listings);
